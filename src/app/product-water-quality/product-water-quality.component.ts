cb31a79aed3705b27a55eb0e34b93bc215f5bf53
Akash <akasharul2101@gmail.com>
2021-07-05 17:36:43 +0530

    improved measurement visualizations

--- src/app/product-water-quality/product-water-quality.component.ts
+++ src/app/product-water-quality/product-water-quality.component.ts
@@ -10,9 +10,9 @@ export class ProductWaterQualityComponent implements OnInit {
 
   values: any[] = [
     {"name": "CONDUCTIVITY", "value": 45, "logo": "assets/ConductivityLogo.png"},
-    {"name": "SALINITY", "value": 45, "logo": "assets/SalinityLogo.png"},
-    {"name": "TURBIDITY", "value": 45, "logo": "assets/TurrbidityLogo.png"},
-    {"name": "PH", "value": 45, "logo": "assets/PHLogo.png"}
+    {"name": "SALINITY", "value": 60, "logo": "assets/SalinityLogo.png"},
+    {"name": "TURBIDITY", "value": 25, "logo": "assets/TurrbidityLogo.png"},
+    {"name": "PH", "value": 75, "logo": "assets/PHLogo.png"}
   ];
 
   locationUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31094.982834751776!2d80.26630104029162!3d13.043764167213897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52689bb60132bb%3A0x549cf8edf62b60a!2sMarina%20Beach!5e0!3m2!1sen!2sin!4v1625484304711!5m2!1sen!2sin";
